b133b710188d9a68fa230871a497b31b7cdd25d7
Ikraam <ikraamghoor@gmail.com>
2020-05-20 12:37:22 +0200

    Completed up to Events:3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,28 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
+    # @current_user = User.find_by_id(session[:current_user_id])
+    @current_user = User.find_by_id(1)
   end
 
   def create
     @user = User.new(signup_params)
     if @user.save
+      session[:current_user_id] = @user.id
       redirect_to @user
     end
-
   end
 
   def show
+    # @current_user = User.find_by_id(session[:current_user_id])
+    @current_user = User.find_by_id(1)
   end
+
   private
+
   def signup_params
-    params.require(:user).permit(:name,:email)
+    params.require(:user).permit(:name, :email)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_091801) do
+ActiveRecord::Schema.define(version: 2020_05_20_101423) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
