94a7a5e670fbccc12e3d4017655c386c02cc42b6
Ikraam <ikraamghoor@gmail.com>
2020-05-21 09:43:23 +0200

    Completed up to Invitations model

---
+++ app/models/invitation.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Invitation < ApplicationRecord
+  belongs_to :attendee, class_name: 'User'
+  belongs_to :attended_event, class_name: 'Event'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  has_many :created_events, foreign_key: "creator", class_name: "Event"
+  has_many :created_events, foreign_key: 'creator', class_name: 'Event'
+  has_many :invitations, foreign_key: :attendee_id
+  has_many :attended_events, through: :invitations, source: :attended_event
 end
